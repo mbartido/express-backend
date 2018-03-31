cb10cf730b90b1d3c566d1493b07a8e97f53d5fd
Michael Bartido <jamberjong@gmail.com>
2018-03-31 15:11:39 -0700

    Can now serve static files from public folder

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,4 +1,10 @@
-import { addNewContact, getContacts, getContactWithID } from '../controllers/crmController';
+import { 
+   addNewContact, 
+   getContacts, 
+   getContactWithID, 
+   updateContact, 
+   deleteContact
+} from '../controllers/crmController';
 
 const routes = (app) => {
    app.route('/contact')
@@ -18,14 +24,10 @@ const routes = (app) => {
    .get(getContactWithID)
 
    // put request
-   .put((req, res) =>
-      res.send('PUT request successful')
-   )
+   .put(updateContact)
    
    // delete request
-   .delete((req, res) =>
-      res.send('DELETE request sucessful')
-   );
+   .delete(deleteContact);
 }
 
 export default routes;
